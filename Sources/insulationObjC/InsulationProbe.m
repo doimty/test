791b13e6fc9ed43366812b49b69b2a11e83b3d48
Mr.She <78968666+doimty@users.noreply.github.com>
2026-06-30 11:44:37 +0800

    probe: bump probe4 telemetry version

--- Sources/insulationObjC/InsulationProbe.m
+++ Sources/insulationObjC/InsulationProbe.m
@@ -35,7 +35,7 @@ static NSMutableDictionary *InsulationProbeState(void) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         state = [NSMutableDictionary dictionary];
-        state[@"version"] = @"0.1.36.3-probe3";
+        state[@"version"] = @"0.1.36.3-probe4";
         state[@"pid"] = @((int)[[NSProcessInfo processInfo] processIdentifier]);
         state[@"processStart"] = @([[NSDate date] timeIntervalSince1970]);
         state[@"events"] = [NSMutableDictionary dictionary];
