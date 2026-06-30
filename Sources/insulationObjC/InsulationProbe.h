5f485342c6587c32eb4782a189c35cedbcae89c3
Mr.She <78968666+doimty@users.noreply.github.com>
2026-06-30 17:42:18 +0800

    probe: record apply source signature

--- Sources/insulationObjC/InsulationProbe.h
+++ Sources/insulationObjC/InsulationProbe.h
@@ -2,7 +2,7 @@
 
 void InsulationProbeMarkLoaded(NSString *stage);
 void InsulationProbeEvent(NSString *event);
-void InsulationProbeRecordApply(NSString *mode, BOOL bootGuardActive);
+void InsulationProbeRecordApply(NSString *mode, BOOL bootGuardActive, NSString *source);
 void InsulationProbeRecordMitigationUpdate(NSString *name, BOOL changed);
 void InsulationProbeRecordSelfHeal(NSString *reason);
 void InsulationProbeRecordSetter(NSString *name, NSInteger originalValue, NSInteger patchedValue);
