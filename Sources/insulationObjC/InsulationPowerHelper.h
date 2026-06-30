3f311e2590e2dda2a49a4c60b38f05759d9bb624
Mr.She <78968666+doimty@users.noreply.github.com>
2026-06-30 17:42:10 +0800

    probe: declare sourced apply entry points

--- Sources/insulationObjC/InsulationPowerHelper.h
+++ Sources/insulationObjC/InsulationPowerHelper.h
@@ -31,6 +31,8 @@ int InsulationMaxComponentPower(int power, int mitigationType);
 void InsulationSetCommonProductObject(CommonProduct *_Nullable product);
 void InsulationSetMitigationControllerObject(MitigationController *_Nullable controller);
 void InsulationExecutePuppetEvent(void);
+void InsulationExecutePuppetEventWithSource(NSString *_Nullable source);
 void InsulationExecutePuppetEventSoon(void);
+void InsulationExecutePuppetEventSoonWithSource(NSString *_Nullable source);
 
 NS_ASSUME_NONNULL_END
